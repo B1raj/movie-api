e8d9a12afdabac03cba0bb390da33dd1ad6fc435
Biraj Kumar Mishra <birajmishra@hotmail.com>
2022-07-14 23:17:03 +0800

    refactored the code

--- src/main/java/com/biraj/backbase/movie/movieapi/constant/MovieConstant.java
+++ src/main/java/com/biraj/backbase/movie/movieapi/constant/MovieConstant.java
@@ -17,12 +17,11 @@ public interface MovieConstant {
 	String AUTHORIZATION = "Authorization";
 	String UUID = "uuid";
 	String USERID = "userId";
-	String INVALID_MOVIE_REQUEST = "Invalid request, please try with valid movie name.";
-	String FORBIDDEN = "You don't have access to the requested resource.";
+	String INVALID_MOVIE_REQUEST = "Invalid request, please try with valid movie name and year.";
 	String BAD_REQUEST = "Bad Request, Please recheck the request.";
 	String RELOGIN = "Unauthorized Access, Please relogin.";
 	String MOVIE = "movie";
 	String YEAR = "year";
-	String CANNOT_SAVE_RATING_MOVIE_NAME_INCORRECT = "Bad Request, Cannot Save Rating: Movie name is incorrect";
-	String CANNOT_SAVE_RATING_USER_DOESNOT_EXIST = "Bad Request, Cannot Save Rating: User doesnot exist";
+	String CANNOT_SAVE_RATING_MOVIE_NAME_INCORRECT = "Bad Request, Cannot Save Rating: Movie name or Year  is incorrect";
+	String CANNOT_SAVE_RATING_USER_DOESNOT_EXIST = "Bad Request, Cannot Save Rating: User does not exist";
 }
